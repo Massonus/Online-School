0b011ab7d6d29abe94a15c3bb541d1f49c2e7fcd
Massonus <maksym.lithuanian@gmail.com>
2023-04-02 22:18:02 +0300

    hw10

--- src/utils/LectureUtils.java
+++ src/utils/LectureUtils.java
@@ -5,9 +5,9 @@ import entity.Lecture;
 public class LectureUtils {
 
     public void CreateLectures() {
-        Lecture lecture = new Lecture(1, "Math");
-        Lecture lecture1 = new Lecture(2, "Physics");
-        Lecture lecture2 = new Lecture(3, "Ukrainian");
+        Lecture lecture = new Lecture();
+        Lecture lecture1 = new Lecture();
+        Lecture lecture2 = new Lecture();
 
     }
 
