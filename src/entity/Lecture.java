0b8712e4524c10c1a8a2eb6cbf19563ee137cdc8
Massonus <maksym.lithuanian@gmail.com>
2023-04-09 14:16:07 +0300

    hw10

--- src/entity/Lecture.java
+++ src/entity/Lecture.java
@@ -6,6 +6,10 @@ public class Lecture extends University {
 
     private int personId;
 
+    private String name;
+
+    private String description;
+
 
      public Lecture(int id, String name, int personId) {
         super(id, name);
@@ -14,5 +18,23 @@ public class Lecture extends University {
     }
 
     public Lecture() {
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
